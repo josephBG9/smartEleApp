b95ed76b670682c9e87e10fc2132ee228caf8f03
joseph9 <bggoo@ut.ac.kr>
2021-11-07 15:33:57 +0900

    Network selection between wifi and mobile is not success. But for referencing, I commit this un-fully functional version.

--- app/src/main/java/kr/ac/ut/smartelevator/app/MainActivity.java
+++ app/src/main/java/kr/ac/ut/smartelevator/app/MainActivity.java
@@ -3,12 +3,20 @@ package kr.ac.ut.smartelevator.app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.os.HandlerCompat;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.telephony.CarrierConfigManager;
 import android.util.Log;
+import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.ExecutorService;
@@ -18,25 +26,56 @@ import kr.ac.ut.smartelevator.common.HandlerCallback;
 import kr.ac.ut.smartelevator.sock.SockClient;
 
 public class  MainActivity extends AppCompatActivity implements HandlerCallback {
-
+    SockClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        Log.i("ELEVATOR","App is started.");
+
         Handler handler = HandlerCompat.createAsync(Looper.getMainLooper());
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-        SockClient client = new SockClient(executorService, handler, this);
+        client = new SockClient(executorService, handler, this, this);
         client.getElevatorErrorCode("192.168.5.5", 5000);
+        //client.getElevatorErrorCode("210.119.145.6", 80);
+/*
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        for(Network network : connMgr.getAllNetworks()) {
+            NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+            if(networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                Log.i("ELEVATOR","Mobile Netork Type : binding process to this network");
+                connMgr.bindProcessToNetwork(network);
+            }
+        } */
+
+
+
 
 
     }
 
     @Override
     public void handleMessage(Message msg) {
+        Log.i("ELEVATOR","Handler.....");
         if(msg.what == HandlerCallback.ELEVATOR_ERR_CODE) {
-            Log.i("ELEVATOR ERROR ", ((JSONObject)msg.obj).toString());
+            JSONObject obj = (JSONObject)msg.obj;
+            try {
+                JSONArray erlist = obj.getJSONArray("lift_err");
+                Log.i("ELEVATOR","N of Error Code in Array : " + erlist.length());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("ELEVATOR", ((JSONObject)msg.obj).toString());
         }
 
+    }
+
+    public void onClick(View v) {
+        client.getElevatorErrorCode("192.168.5.5", 5000);
+        //client.getElevatorErrorCode("210.119.145.6", 80);
     }
 }
--- app/src/main/java/kr/ac/ut/smartelevator/sock/SockClient.java
+++ app/src/main/java/kr/ac/ut/smartelevator/sock/SockClient.java
@@ -1,14 +1,26 @@
 package kr.ac.ut.smartelevator.sock;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -29,10 +41,58 @@ public class SockClient {
     private ExecutorService executorService;
     private HandlerCallback msgProc;
 
-    public SockClient(ExecutorService executorService, Handler handler, HandlerCallback proc) {
+    private boolean flag;
+    private ConnectivityManager connMgr;
+    private Object syncObj;
+    private WifiManager wifiManager;
+
+    public SockClient(ExecutorService executorService, Handler handler, HandlerCallback proc, Context context){ // }, ConnectivityManager conn) {
         this.executorService = executorService;
         this.handler = handler;
         msgProc = proc;
+
+       flag = false;
+       connMgr = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+       syncObj = new Object();
+       wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+
+        NetworkRequest.Builder builder = new NetworkRequest.Builder();
+        connMgr.requestNetwork(builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI).build(),
+                new ConnectivityManager.NetworkCallback(){
+                    @Override
+                    public void onAvailable(@NonNull Network network) {
+                        super.onAvailable(network);
+                        Log.i("ELEVATOR","Wifi Network is available.");
+                        Log.i("ELEVATOR", "internet(wifi) connection : " + connMgr.isActiveNetworkMetered());
+                    }
+
+                    @Override
+                    public void onLost(@NonNull Network network) {
+                        super.onLost(network);
+                        Log.i("ELEVATOR","Wifi Network is unavailable.");
+                        //connMgr.unregisterNetworkCallback(this);
+                    }
+                });
+
+        NetworkRequest.Builder cbuilder = new NetworkRequest.Builder();
+        connMgr.requestNetwork(cbuilder.addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR).build(),
+                new ConnectivityManager.NetworkCallback(){
+                    @Override
+                    public void onAvailable(@NonNull Network network) {
+                        super.onAvailable(network);
+                        Log.i("ELEVATOR","Cellular Network is available.");
+                        Log.i("ELEVATOR", "internet(cellular) connection : " + connMgr.isActiveNetworkMetered());
+                    }
+
+                    @Override
+                    public void onLost(@NonNull Network network) {
+                        super.onLost(network);
+                        Log.i("ELEVATOR","Cellular Network is unavailable.");
+                        //connMgr.unregisterNetworkCallback(this);
+                    }
+                });
+
+
     }
 
     public String getErrorDate(byte[] packet, int idx) {
@@ -65,6 +125,10 @@ public class SockClient {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
+
+                Network mobileNetwork = null;
+                Socket csocket = null;
+
                 byte[] reqData = { (byte) 0xA5, (byte) 0x5A, (byte) 0x09, (byte) 0x00,
                         (byte) 0x21, (byte)0x18, (byte)0x0B, (byte) 0x0D, (byte) 0x0A };
                 byte[] resData = new byte[1024];
@@ -78,8 +142,93 @@ public class SockClient {
                     Log.i("SOCKET", "JSONObject put() array error : " + e);
                 }
 
+                if(!flag) {
+                    flag = true;
+                    Log.i("ELEVATOR", "WIFI Network is used.");
+
+
+
+                }
+                else {
+                    flag = false;
+                    for(Network network : connMgr.getAllNetworks()) {
+
+                        Log.i("ELEVATOR","Network Type : " + connMgr.getNetworkCapabilities(network).toString());
+
+                        if(connMgr.getNetworkCapabilities(network).hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                            Log.i("ELEVATOR","Mobile Network Type : binding process to this network");
+
+                            try {
+                                //csocket = network.getSocketFactory().createSocket(ipaddr, port);
+                                connMgr.bindProcessToNetwork(network); // for bindProcessToNetwork()
+                                Log.i("ELEVATOR","IPADDR : " + ipaddr + "\tPort : " + port);
+                                InetSocketAddress inetSocketAddress = new InetSocketAddress(ipaddr, port);
+                                csocket = new Socket(ipaddr, port);// for bindProcessToNetwork()
+                                // csocket = new Socket();
+                                csocket.connect(inetSocketAddress, 10000);
+
+                                Log.i("ELEVATOR","new socket : " + csocket);
+
+
+
+                            } catch (IOException e) {
+                                Log.i("ELEVATOR","getSocketFactory() Error : " + e.toString());
+                                e.printStackTrace();
+                                csocket = null;
+                            }
+                            break;
+                        }
+
+                        //NetworkInfo info = connMgr.getNetworkInfo(network);
+                        //if(info.getType() == ConnectivityManager.TYPE_MOBILE) {
+                        //    Log.i("ELEVATOR","Mobile Netork Type : binding process to this network");
+
+                        //    NetworkRequest.Builder builder = new NetworkRequest.Builder();
+                        //    builder.addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR);
+
+                            //Log.i("ELEVATOR", "WIFI disabled");
+                            //wifiManager.setWifiEnabled(false);
+
+                            //connMgr.requestNetwork(builder.build(), new ConnectivityManager.NetworkCallback(){
+                            //    @Override
+                            //    public void onAvailable(@NonNull Network network) {
+                            //        super.onAvailable(network);
+                            //        connMgr.bindProcessToNetwork(network);
+
+
+                            //        synchronized (syncObj) {
+                            //            syncObj.notify();
+                            //        }
+                            //        connMgr.unregisterNetworkCallback(this);
+                            //    }
+                            //} );
+
+                            //mobileNetwork = network;
+                            //try {
+                            //    Log.i("ELEVATOR","Wait until network is available.");
+                            //    synchronized (syncObj) {
+                            //        syncObj.wait();
+                            //    }
+                            //    Log.i("ELEVATOR","awaked.");
+                            //} catch (InterruptedException e) {
+                            //    e.printStackTrace();
+                            //}
+                            //break;
+                       // }
+                    }
+                }
+
+
                 try {
-                    client = new Socket(ipaddr, port);
+                    Log.i("ELEVATOR","Connection request..."  );
+                    if(csocket == null) {
+                        Log.i("ELEVATOR","Original socket creation.");
+                        client = new Socket(ipaddr, port);
+                    }
+                    else {
+                        Log.i("ELEVATOR", "New socket");
+                        client = csocket;
+                    }
 
                     client.getOutputStream().write(reqData);
                     client.getOutputStream().flush();
@@ -96,12 +245,18 @@ public class SockClient {
                             // 여기는 추가적으로 오류 검사를 할 수 있어야 함.
                             readBytes += client.getInputStream().read(resData);
                         }
-                        if(resData[SockClient.ERROR_COUNT] == 0)
+
+                        if(resData[SockClient.ERROR_COUNT] == 0) {
+                            // 현재는 마지막 패킷은 오류 코드의 수가 125보다 적은 경우
+                            // 하지만, 마지막 패킷에 오류 코드가 125개인 경우 마지막인지를 알 수 없음.
+                            // 따라서, 이 경우 오류 코드의 수가 0인 패킷을 보낸다고 가정하고 작업.
+                            // 향후 협의가 필요함.
                             break;
+                        }
 
                         jsonObject.put("lift_id", getElevatorID(resData));
                         Log.i("ELEVATOR","n of Code : " + resData[SockClient.ERROR_COUNT]);
-                        Log.i("ELEVATOR", "*************************************");
+                        // Log.i("ELEVATOR", "*************************************");
                         for(int j=0; j<resData[SockClient.ERROR_COUNT]; j++) {
                             errData = new JSONObject();
                             errData.put("date", getErrorDate(resData, j));
@@ -112,7 +267,14 @@ public class SockClient {
                             jsonArray.put(errData);
                         }
 
+                        Log.i("ELELVATOR", "N of Error Data in Array : " + jsonArray.length());
+
+                        if(resData[SockClient.ERROR_COUNT] < 125)
+                            break;
                     }
+                    if(client != null)
+                        client.close();
+                    connMgr.bindProcessToNetwork(null);
 
                     Message msg = new Message();
                     msg.what = HandlerCallback.ELEVATOR_ERR_CODE;
@@ -124,17 +286,17 @@ public class SockClient {
                         }
                     });
 
+                    Log.i("ELEVATOR","Done - reading error code");
+
                 } catch (IOException e) {
                     client = null;
-                    Log.i("SOCKET", "Socket Proc. error! : " + e);
+                    Log.i("ELEVATOR", "Socket Proc. error! : " + e);
                 } catch (JSONException e) {
-                    Log.i("SOCKET", "JSONObject put() error : " + e);
+                    Log.i("ELEVATOR", "JSONObject put() error : " + e);
                 }
 
             }
         });
-
-
     }
 
 }

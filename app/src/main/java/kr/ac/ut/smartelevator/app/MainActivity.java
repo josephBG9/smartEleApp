bbf13d0b62228ff6edd9869fe7b9a969028e2f1a
joseph9 <bggoo@ut.ac.kr>
2021-11-08 12:25:32 +0900

    Module for temporarily stroing JSON data into internal file; first version

--- app/src/main/java/kr/ac/ut/smartelevator/app/MainActivity.java
+++ app/src/main/java/kr/ac/ut/smartelevator/app/MainActivity.java
@@ -1,14 +1,80 @@
 package kr.ac.ut.smartelevator.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.telephony.CarrierConfigManager;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import kr.ac.ut.smartelevator.buffer.BufferMgr;
+
+
+public class  MainActivity extends AppCompatActivity {
+
+    BufferMgr buffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buffer = new BufferMgr(this);
+
+    }
+
+    public void onClick(View v) {
+        Random rand = new Random();
+        if(v == findViewById(R.id.button)) {
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("data", rand.nextInt(100));
+                obj.put("type", rand.nextInt(2));
+                buffer.saveData(obj.getInt("type"), obj);
+                Log.i("ELEVATOR", "json obj : " + obj.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        } else if(v == findViewById(R.id.button2)) {
+            JSONArray array;
+            array = buffer.loadData(0);
+            Log.i("ELEVATOR","length type 0 : " + array.length());
+            for(int i=0; i<array.length(); i++) {
+                try {
+                    Log.i("ELEVATOR", "data(type 0) : " + ((JSONObject)array.get(i)).toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            array = buffer.loadData(1);
+            Log.i("ELEVATOR","length type 1 : " + array.length());
+            for(int i=0; i<array.length(); i++) {
+                try {
+                    Log.i("ELEVATOR", "data(type 1) : " + ((JSONObject)array.get(i)).toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/kr/ac/ut/smartelevator/buffer/BufferMgr.java
@@ -0,0 +1,72 @@
+package kr.ac.ut.smartelevator.buffer;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.Stream;
+
+public class BufferMgr {
+    public static final int TYPE_ELEVATOR_ERROR = 1;
+    public static final int TYPE_ELEVATOR_MAINTENANCE = 2;
+    private Context context;
+
+    public BufferMgr(Context context) {
+        this.context = context;
+    }
+
+    public void saveData(int type, JSONObject obj) {
+        BufferedWriter writer;
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    context.openFileOutput("data." + type,
+                            Context.MODE_PRIVATE | Context.MODE_APPEND)));
+
+            writer.write(obj.toString() +"\n");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i("EVEVATOR", "BufferMgr - File open Error in saveData");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("EVEVATOR", "BufferMgr - File IO Error in saveData");
+        }
+    }
+
+    public JSONArray loadData(int type) {
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+        BufferedReader reader;
+        String line;
+
+        jsonArray = new JSONArray();
+        try {
+            reader = new BufferedReader(new InputStreamReader(
+                    context.openFileInput("data."+type)));
+            while((line=reader.readLine()) != null) {
+                jsonArray.put(new JSONObject(line));
+            }
+            reader.close();
+            context.deleteFile("data."+type);
+
+        } catch (FileNotFoundException e) {
+            // No operation
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jsonArray;
+    }
+}

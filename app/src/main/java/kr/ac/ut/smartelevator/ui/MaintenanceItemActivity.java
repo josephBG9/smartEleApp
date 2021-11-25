cd8f7538a32864ad67b92da0fbf0af49d2380b2e
joseph9 <bggoo@ut.ac.kr>
2021-11-25 19:15:42 +0900

    Socket Service Integrating.

--- app/src/main/java/kr/ac/ut/smartelevator/ui/MaintenanceItemActivity.java
+++ app/src/main/java/kr/ac/ut/smartelevator/ui/MaintenanceItemActivity.java
@@ -1,17 +1,25 @@
 package kr.ac.ut.smartelevator.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
 
 import kr.ac.ut.smartelevator.app.R;
+import kr.ac.ut.smartelevator.sock.SockClient;
 
-public class MaintenanceItemActivity extends AppCompatActivity {
+public class MaintenanceItemActivity extends AppCompatActivity implements Handler.Callback{
+
+    ExecutorService executorService;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,11 @@ public class MaintenanceItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maintenance_item);
 
         Intent intent = getIntent();
+
+        executorService = ExecutorService.
+        SockClient sockClient = new SockClient();
+
+
 
         TextView tvna = (TextView) findViewById(R.id.ele_name_id);
         tvna.setText(intent.getStringExtra("lift_name") + "("
@@ -31,5 +44,10 @@ public class MaintenanceItemActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean handleMessage(@NonNull Message msg) {
+        return false;
     }
 }
